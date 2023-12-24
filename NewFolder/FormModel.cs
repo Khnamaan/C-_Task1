360402e37518aebb30f4d0adcd5ca52b7fd4388a
Khnamaan <amaan6509@gmail.com>
2023-12-24 07:59:37 +0545

    task_1

---
+++ NewFolder/FormModel.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProjectUNODesktop.NewFolder
+{
+    internal class FormModel
+    {
+        public int ID { get; set; }
+        public string Task {  get; set; }
+        public bool IsCompleted { get; set; }
+    }
+}
